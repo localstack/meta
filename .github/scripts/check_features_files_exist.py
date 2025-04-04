34d8db0966a3175ddd1c66ed5de1c999421eac61
Anastasia Dusak <61540676+k-a-il@users.noreply.github.com>
2025-04-04 13:52:18 +0200

    Validate files containing descriptions of AWS services (#15)

---
+++ .github/scripts/check_features_files_exist.py
@@ -0,0 +1,44 @@
+import os
+import sys
+
+FEATURES_FILE_NAME='features.yml'
+
+def find_paths_to_service_providers(directory: str) -> list[str]:
+    provider_path = os.path.join(directory, 'provider.py')
+    if os.path.isfile(provider_path):
+        return [provider_path]
+    paths = []
+    for root, _, files in os.walk(directory):
+        if 'provider.py' in files:
+            paths.append(os.path.join(root, 'provider.py'))
+    return paths
+
+
+def map_features_files_status(services_path, changed_files) -> dict[str, bool]:
+    features_files_exist_status = {}
+    for file_path in changed_files:
+        if file_path.startswith(services_path):
+            service_folder_name = file_path.removeprefix(services_path).split('/')[1]
+            service_path = os.path.join(services_path, service_folder_name)
+
+            provider_files = find_paths_to_service_providers(service_path)
+            for provider_file in provider_files:
+                features_file = os.path.join(os.path.dirname(provider_file), FEATURES_FILE_NAME)
+                features_files_exist_status[features_file] = os.path.exists(features_file)
+    return features_files_exist_status
+
+def main():
+    # Detect changed features files
+    comma_separated_changed_files = os.getenv('ALL_CHANGED_FILES')
+    changed_files = comma_separated_changed_files.split(',')
+
+    #Check features file exists in services folder
+    services_path = os.getenv('SERVICES_PATH')
+    features_file_status = map_features_files_status(services_path, changed_files)
+    for file_path, exists in features_file_status.items():
+        if not exists:
+            sys.stdout.write(f"::error title=Features file is missing::Service code was changed but no corresponding feature file found at {file_path}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ .github/scripts/validate_features_files.py
@@ -0,0 +1,69 @@
+import json
+import os
+
+import yaml
+from jsonschema import validate, ValidationError
+import sys
+
+FEATURES_FILE_NAME='features.yml'
+
+def load_yaml_file(file_path: str):
+    try:
+        with open(file_path, 'r') as file:
+            return yaml.safe_load(file)
+    except yaml.YAMLError as e:
+        print(f"Error parsing YAML file: {e}")
+        sys.exit(1)
+    except FileNotFoundError:
+        print(f"YAML file not found: {file_path}")
+        sys.exit(1)
+
+def load_json_file(file_path: str):
+    try:
+        with open(file_path, 'r') as file:
+            return json.load(file)
+    except json.JSONDecodeError as e:
+        print(f"Failed to parse JSON schema file: {e}")
+        sys.exit(1)
+    except FileNotFoundError:
+        print(f"Json schema file not found: {file_path}")
+        sys.exit(1)
+
+def validate_yaml_against_schema(yaml_data: str, json_schema: str, file_path: str) -> bool:
+    try:
+        validate(instance=yaml_data, schema=json_schema)
+        print(f"Successful validation of file: {file_path}")
+        return True
+    except ValidationError as e:
+        sys.stdout.write(f"::error file={file_path},title=Validation has failed at path {' -> '.join(str(x) for x in e.path)}::{e.message}")
+        return False
+
+def main():
+    # Detect changed features files
+    comma_separated_changed_files = os.getenv('ALL_CHANGED_FILES')
+    if not comma_separated_changed_files:
+        print("Environment variable ALL_CHANGED_FILES is not set. Skipping validation.")
+        sys.exit(1)
+
+    changed_files = comma_separated_changed_files.split(',')
+    changed_features_files = [path for path in changed_files if path.lower().endswith(FEATURES_FILE_NAME)]
+    print(f'Changed features files: {",".join(changed_features_files)}')
+
+    if len(changed_features_files) == 0:
+        print('No feature file was changed. Skipping validation.')
+        sys.exit(0)
+
+    features_schema_path = os.getenv('FEATURES_JSON_SCHEMA', 'features_schema.json')
+    features_schema = load_json_file(features_schema_path)
+    print(f'Features schema file was loaded: {features_schema_path}')
+
+    # validate schemas
+    all_valid = True
+    for file_path in changed_features_files:
+        features_file = load_yaml_file(file_path)
+        if not validate_yaml_against_schema(features_file, features_schema, file_path):
+            all_valid = False
+    sys.exit(0 if all_valid else 1)
+
+if __name__ == "__main__":
+    main()
